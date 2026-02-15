ed7fca3bc73b633f21c4a41cb9ec417feb24bcf4
servaltullius <servaltullius@users.noreply.github.com>
2026-02-16 01:44:46 +0900

    refactor: split action dispatch into dedicated module

--- skse/CalamityAffixes/src/EventBridge.Actions.cpp
+++ skse/CalamityAffixes/src/EventBridge.Actions.cpp
@@ -11,42 +11,4 @@ namespace CalamityAffixes
 			console->Print(msg.c_str());
 		}
 	}
-
-	void EventBridge::DispatchActionByType(const AffixRuntime& a_affix, RE::Actor* a_owner, RE::Actor* a_target, const RE::HitData* a_hitData)
-	{
-		const auto& a_action = a_affix.action;
-		switch (a_action.type) {
-		case ActionType::kDebugNotify:
-			ExecuteDebugNotifyAction(a_action);
-			break;
-		case ActionType::kCastSpell:
-			ExecuteCastSpellAction(a_affix, a_owner, a_target, a_hitData);
-			break;
-		case ActionType::kCastSpellAdaptiveElement:
-			ExecuteCastSpellAdaptiveElementAction(a_action, a_owner, a_target, a_hitData);
-			break;
-		case ActionType::kSpawnTrap:
-			ExecuteSpawnTrapAction(a_action, a_owner, a_target, a_hitData);
-			break;
-		default:
-			break;
-		}
-	}
-
-	void EventBridge::ExecuteActionWithProcDepthGuard(const AffixRuntime& a_affix, RE::Actor* a_owner, RE::Actor* a_target, const RE::HitData* a_hitData)
-	{
-		_procDepth += 1;
-		ExecuteAction(a_affix, a_owner, a_target, a_hitData);
-		_procDepth -= 1;
-	}
-
-	void EventBridge::ExecuteAction(const AffixRuntime& a_affix, RE::Actor* a_owner, RE::Actor* a_target, const RE::HitData* a_hitData)
-	{
-		if (!a_owner) {
-			return;
-		}
-
-		DispatchActionByType(a_affix, a_owner, a_target, a_hitData);
-	}
-
 }
